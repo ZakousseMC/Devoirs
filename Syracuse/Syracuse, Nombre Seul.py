c5756be8c61cd237597ecac0f700c81f3ff90b10
Zaky <zakoussemc@gmail.com>
2018-12-23 03:10:51 +0400

    Organisation

---
+++ Syracuse/Syracuse, Nombre Seul.py
@@ -0,0 +1,35 @@
+from math import *
+import sys
+
+#TAILLE MAXIMUM D'UNE LISTE / STR.
+print("La valeur maximum pour les strings ou liste est de :",sys.maxsize)
+
+#DEF PAIRIMPAIR
+def PairImpair(n):
+
+    if n % 2 == 0:
+        return("pair")
+    else:
+        return("impair")
+
+n=int(input("Entrez le nombre que vous vouliez tester, il doit être entier : "))
+print("Le nombre ",n," est ",PairImpair(n))
+
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+        print(n)
+        if n == 4:
+            print("Lorsque n = 4, il est impossible de continuer, fin du programme.")
+            sys.exit(".") #Fin du programme.
+    else:
+        n = 3*n+1
+        print (n)
+        if n == 4:
+            print("Lorsque n = 4, il est impossible de continuer, fin du programme.")
+            sys.exit(".") #Fin du programme.
+
+#CONTOURNER OVERFLOW SYS ADD 2 SLASH/IMPORT SYS
+#Python peut gérer des nombres entiers arbitrairement grands lors du calcul.
+#Tout nombre entier trop grand pour tenir sur 64 bits (ou quelle que soit la limite matérielle sous-jacente) est géré par logiciel.
+#Pour cette raison, Python 3 n'a pas de constante sys.maxint.
