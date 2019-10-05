54aa4fdb187c9a7e7dc8b7c664d79d56eefcf287
Zaky <zakoussemc@gmail.com>
2019-10-05 19:28:09 +0400

    Ajout d'un programme test de conditions nb premier
    Vérifie si un nombre est premier ou non.

---
+++ Vérification d'un nombre, premier ou pas Python.py
@@ -0,0 +1,15 @@
+from math import *
+
+#Initalisation des variables.
+x = int(input("Saisissez un nombre :"))
+i = 2
+
+#Début de la boucle.
+for z in range(x):
+	while i < n and n % i != 0 :
+	    i = i + 1
+	if i == n :
+	    print("Le nombre est premier")
+	else :
+	    print("Le nombre est pas premier.")
+
